810b95f7740395f657ffe8f76452088df5e31462
ngovanquy <ngoq753429@gmail.com>
2024-12-13 20:02:25 +0700

    feat: Create feature upload single and multiple files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import UploadMultiple from "./components/UploadMultiple";
+import UploadSingle from "./components/UploadSingle";
+
+function App() {
+  return (
+    <>
+      {/* <UploadSingle /> */}
+      <UploadMultiple />
+    </>
+  );
+}
+
+export default App;
